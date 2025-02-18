2d18579fa1f7aaa2ffdcc3031259ff580c1d2083
krishna <krishnasubramaiyan2001@gmail.com>
2025-02-18 19:55:19 +0530

    member initializer correct constructor

--- member_initializer.cpp
+++ member_initializer.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//member initializer solve another class object creation problem initialize incoming value
+//member initializer solve another class object creation problem initialize using incoming value
 //class dob
 class BirthDay{
     short day,month,year;
@@ -11,8 +11,8 @@ public:
     year = y;
     }
 
-     void getDOB(){
-        cout << day << "/" << month << "/" << year;
+     string getDOB(){
+         return to_string(day)+"/"+to_string(month)+"/"+to_string(year);
     }
 };
 
@@ -20,26 +20,21 @@ class Person{
     string name;
     BirthDay dob;
 public:
-    Person(string n, BirthDay obj) : name(n), dob(obj){
+    Person(string n, short day, short month, short year): name(n),dob(day,month,year){
     }
 
     void personDetails(){
         cout << endl << "Name : " << name << endl;
-        cout << "DOB  : ";
-        dob.getDOB();
-        cout << endl;
+        cout << "DOB  : " << dob.getDOB() << endl;
     }
 };
 
 int main(){
 
-    BirthDay dob1(1,1,2000);
-    Person user1("User 1",dob1);
+    Person user1("user 1",1,1,2000);
+    Person user2("user 2",2,2,2000);
+
     user1.personDetails();
-
-    BirthDay dob2(1,1,2000);
-    Person user2("User 2",dob2);
     user2.personDetails();
-
     return 0;
 }
